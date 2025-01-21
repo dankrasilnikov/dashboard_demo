b8088c8fa2c64ce8bbb983b692a64f7d1631e7e4
dankrasilnikov <krasilnikov.orchid@gmail.com>
2025-01-21 23:08:42 +0100

    fix: button alignment

--- src/routes/demo/components/WidgetHeadings/SwitchTicketWidgetHeading.svelte
+++ src/routes/demo/components/WidgetHeadings/SwitchTicketWidgetHeading.svelte
@@ -17,7 +17,7 @@
     </div>
 
     <div class={`flex flex-row ${mobileWrap ? 'justify-start md:justify-center md:items-center mt-2 md:mt-0' : 'justify-center items-center'}`}>
-        <Button className="!rounded-r-none !rounded-l !text-xs !py-1 !px-3 !text-[#6B7280]" strokebtn={true}>Ticket Sales</Button>
+        <Button className="!rounded-r-none !rounded-l !text-xs !py-1 !px-3 !text-[#6B7280] !whitespace-nowrap" strokebtn={true}>Ticket Sales</Button>
         <Button className="!rounded-l-none !rounded-r !text-xs !py-1 !px-3 !bg-primary-500 border-primary-500 border-[1px]">Revenue</Button>
     </div>
 </div>
